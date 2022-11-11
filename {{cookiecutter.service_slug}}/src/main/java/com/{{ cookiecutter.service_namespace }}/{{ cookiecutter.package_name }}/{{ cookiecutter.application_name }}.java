f475f70b0236bb75f3d7c7b8b90e20d63c6cf868
brian <brian.rook@gmail.com>
2022-11-11 14:34:44 -0700

    add active profile to test

--- {{cookiecutter.service_slug}}/src/main/java/com/{{ cookiecutter.service_namespace }}/{{ cookiecutter.package_name }}/{{ cookiecutter.application_name }}.java
+++ {{cookiecutter.service_slug}}/src/main/java/com/{{ cookiecutter.service_namespace }}/{{ cookiecutter.package_name }}/{{ cookiecutter.application_name }}.java
@@ -1,4 +1,4 @@
-package com.bullyrooks.{{ cookiecutter.package_name }};
+package com.{{ cookiecutter.service_namespace }}.{{ cookiecutter.package_name }};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
